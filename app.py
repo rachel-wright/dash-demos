31bc3fe8f24edf9c1357bf63991426533d3710c3
Rachel Wright <35343959+rachel-wright@users.noreply.github.com>
2019-02-05 13:13:26 -0500

    Update and rename app.py~ to app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import flask
+import dash
+
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server)
+
+app.layout = html.Div([
+    html.H2(children='Hello, Dash')
+ ])
+ 
+ if __name__ == '__main__':
+    app.run_server(debug=True)
