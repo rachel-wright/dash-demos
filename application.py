a0c6cf619a3f99cee0e2f5ea12d296dca93f5dcc
Rachel Wright <35343959+rachel-wright@users.noreply.github.com>
2019-02-05 13:07:29 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
-from flask import Flask
+import dash
 
-app = Flask(__name__)
+app = dash.Dash(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app.layout = html.Div([
+    html.H2(children='Hello, World!')
+ ])
+
+if __name__ == '__main__':
+    app.run_server()
+
